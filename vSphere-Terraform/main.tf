c07149b02aee367f5c003e3db3a0b78ef2b35400
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2022-06-11 20:55:16 +0530

    Update main.tf

--- vSphere-Terraform/main.tf
+++ vSphere-Terraform/main.tf
@@ -6,3 +6,11 @@ provider "vsphere" {
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
+
+data "vsphere_datacenter" "dc" {}
+
+resource "vsphere_folder" "folder" {
+  path          = var.folder_name
+  type          = "vm"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
