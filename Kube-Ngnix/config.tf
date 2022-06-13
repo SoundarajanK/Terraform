6ea9fe1be4a1de0345e93050f6ecddbeb699482f
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2022-06-13 16:10:55 +0530

    Update config.tf

--- Kube-Ngnix/config.tf
+++ Kube-Ngnix/config.tf
@@ -1,8 +1,8 @@
 provider "kubernetes" {
   host=var.kube_ip
   version="~> 1.13"
-  client_certificate = file("ca.crt")
+  client_certificate = file("server.crt")
   client_key = file("server.key")
-  cluster_ca_certificate = file("server.crt")
+  cluster_ca_certificate = file("ca.crt")
   load_config_file = false
 }
