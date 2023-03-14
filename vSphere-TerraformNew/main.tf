02ad4255d5831a571548f0bd50141a8438371297
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-14 19:15:38 +0530

    Update main.tf

--- vSphere-TerraformNew/main.tf
+++ vSphere-TerraformNew/main.tf
@@ -11,6 +11,11 @@ data "vsphere_datacenter" "datacenter" {
   name = "S360-MG-DC01"
 }
 
+data "vsphere_resource_pool" "pool" {
+  name          = "S360-MG-CL01/Mgmt-rp"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 data "vsphere_compute_cluster" "compute_cluster" {
   name          = "S360-MG-CL01"
   datacenter_id = data.vsphere_datacenter.datacenter.id
@@ -20,4 +25,5 @@ resource "vsphere_folder" "folder" {
   path          = var.folder_name
   type          = "vm"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
 }
