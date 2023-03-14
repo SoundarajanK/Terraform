3424ad8a7c675f3aedf82dc168673c0988657d99
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-15 03:31:08 +0530

    Update main.tf

--- vSphere-TerraformNew/main.tf
+++ vSphere-TerraformNew/main.tf
@@ -9,8 +9,14 @@ provider "vsphere" {
 
 data "vsphere_datacenter" "dc" {}
 
+data "vsphere_resource_pool" "resource_pool" {
+  name              = "/${var.vsphere_vm_datacenter}/host/${var.vsphere_vm_compute_cluster}/Resources/${var.vsphere_vm_resource_pool}"
+  datacenter_id     = "${data.vsphere_datacenter.dc.id}"
+}
+
 resource "vsphere_folder" "folder" {
   path          = var.folder_name
   type          = "vm"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  resource_pool_id    = "${data.vsphere_resource_pool.resource_pool.id}"
 }
