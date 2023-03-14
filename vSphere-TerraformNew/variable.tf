0c111c5a5697fff2a496dd571d8d3abdc502aa48
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-15 03:50:32 +0530

    Update variable.tf

--- vSphere-TerraformNew/variable.tf
+++ vSphere-TerraformNew/variable.tf
@@ -1,4 +1,5 @@
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
-variable "folder_name" {}
+variable "vsphere_datacenter_name" {}
+variable "vsphere_cluster_name" {}
