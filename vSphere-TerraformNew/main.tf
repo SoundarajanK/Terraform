012d1ceb5954f5c595ee8334ba6f929a1fdf69ec
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-15 03:51:24 +0530

    Update main.tf

--- vSphere-TerraformNew/main.tf
+++ vSphere-TerraformNew/main.tf
@@ -5,7 +5,7 @@ provider "vsphere" {
 }
 
 resource "vsphere_folder" "my_folder" {
-  path     = "/${var.vsphere_datacenter_name}/host/${var.vsphere_cluster_name}/vm/"
+  path     = "/${var.vsphere_datacenter_name}/host/${var.vsphere_cluster_name}/User-VM/"
   name     = "MyFolder"
   type     = "Folder"
 }
