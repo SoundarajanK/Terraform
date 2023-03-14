1aea96b42870acd4c90b48d943da8f6234fe103a
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-14 18:14:28 +0530

    Update main.tf

--- vSphere-TerraformNew/main.tf
+++ vSphere-TerraformNew/main.tf
@@ -1,4 +1,19 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
 data "vsphere_compute_cluster" "cluster" {
   name              = "${var.vsphere_vm_compute_cluster}"
   datacenter_id     = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_folder" "folder" {
+  path          = var.folder_name
+  type          = "vm"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
