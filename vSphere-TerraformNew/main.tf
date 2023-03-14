58aa5e33ba9b8b756337c8135053ad91e9fd2a6b
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-15 04:01:47 +0530

    Update main.tf

--- vSphere-TerraformNew/main.tf
+++ vSphere-TerraformNew/main.tf
@@ -8,5 +8,5 @@ data "vsphere_datacenter" "dc" {}
 resource "vsphere_folder" "folder" {
   path     = "/${var.vsphere_datacenter_name}/host/${var.vsphere_cluster_name}/User-VM/"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
-  type     = "Folder"
+  type     = "vm"
 }
