10719830b064d0961dc5c95439d4f62f164b9bb0
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-14 20:06:04 +0530

    Update variable.tf

--- vSphere-Terraform/variable.tf
+++ vSphere-Terraform/variable.tf
@@ -1,4 +1,27 @@
-variable "vsphere_user" {}
-variable "vsphere_password" {}
-variable "vsphere_server" {}
-variable "folder_name" {}
+variable "vsphere_user" {
+  description = "vSphere username"
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+}
+
+variable "vsphere_server" {
+  description = "vSphere server address"
+}
+
+variable "vsphere_datacenter_name" {
+  description = "vSphere datacenter name"
+}
+
+variable "vsphere_cluster_name" {
+  description = "vSphere cluster name"
+}
+
+variable "vsphere_folder_name" {
+  description = "Name of the folder to create"
+}
+
+variable "vsphere_folder_path" {
+  description = "Path of the folder to create"
+}
