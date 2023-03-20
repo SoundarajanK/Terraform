d5a3a89c88391ae8bb7cc7ab18c773edfbb1b60e
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-20 13:44:20 +0530

    Create variable.tf

---
+++ kube-nginx-new/variable.tf
@@ -0,0 +1,9 @@
+variable "kube_ip" {
+  description = "IP for Kubernetes"
+  default = "10.108.202.39:16443"
+  }
+  
+variable "nginx_node_port" {
+  description = "NGINX Node Port"
+  default = "30202"
+  }
