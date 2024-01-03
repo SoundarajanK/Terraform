e52bfe7eff7895357c0f77841a4dd949285eaf3a
SoundarajanK <146933065+SoundarajanK@users.noreply.github.com>
2024-01-04 01:16:12 +0530

    Create main.tf

---
+++ VirtualMachine/main.tf
@@ -0,0 +1,39 @@
+data "vsphere_datacenter" "datacenter" {
+  name = "S360-MG-DC01"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "S360-MG-VSAN01"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "S360-MG-CL01"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = "pg-dvmg-dc01-WL-vLAN3241"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "CentOS7-CloudInit"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "Test-VM"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
