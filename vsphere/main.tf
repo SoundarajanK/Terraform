03adef798c09e3345dd8b065c78999c5a71de7bc
SoundarajanK <146933065+SoundarajanK@users.noreply.github.com>
2024-01-31 20:01:35 +0530

    Update main.tf

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -1,39 +1,61 @@
 provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
-data "vsphere_datacenter" "dc" {
+
+data "vsphere_datacenter" "datacenter" {
   name = "S360-MG-DC01"
 }
 
 data "vsphere_datastore" "datastore" {
   name          = "S360-MG-VSAN01"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "S360-MG-CL01"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_network" "network" {
   name          = "pg-dvmg-dc01-WL-vLAN3241"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "CentOS7-CloudInit"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
-  num_cpus = 1
-  memory   = 1024
-  guest_id = "CentOS7-CloudInit"
-
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
-
   disk {
     label = "disk0"
-    size  = 10
+    size  = 20
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
-
-  wait_for_guest_net_timeout    = -1
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+        network_interface {
+        ipv4_address = "192.168.141.29"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "192.168.141.1"
+    }
+  }
 }
